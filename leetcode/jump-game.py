3b1831f4c5a753b850accdcbda5c0629aec04cb9
Ezequiel Buck <ezequiel.bm04@gmail.com>
2025-08-13 22:49:16 -0600

    day 4 part 2

---
+++ leetcode/jump-game.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # key point are the zero's
+        zeros = [i for i, x in enumerate(nums) if x == 0]
+
+        # Last index zero doesn't affect
+        if (len(zeros) > 0) and (zeros[-1] == (len(nums) - 1)):
+            zeros.pop()
+
+        # if no zero's, can always reach the end
+        if len(zeros) == 0:
+            return True
+
+        for i in range(len(zeros) - 1, -1, -1):
+            passes = 0
+            for j in range(zeros[i] - 1, -1, -1):
+                if nums[j] > (zeros[i] - j):
+                    passes = 1
+            if passes == 0:
+                return False
+
+        return True
